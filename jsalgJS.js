dc6b2f5ad94e799c8a1d1006cee3c0e91e9cf24f
Jonathan Salguero <mzootfb@gmail.com>
2018-07-31 08:56:10 -0400

    Add files via upload

---
+++ jsalgJS.js
@@ -0,0 +1,48 @@
+$("#fullbtn").click(function(){
+	$("#sldrFall").attr("value", "12");
+	$("#sldrSpr").attr("value", "12");
+	$("#sldrSum").attr("value", "6");
+});
+
+$("#partbtn").click(function(){
+	$("#sldrFall").attr("value", "6");
+	$("#sldrSpr").attr("value", "6");
+	$("#sldrSum").attr("value", "3");
+});
+
+$("#custbtn").click(function(){
+	$("#sldrFall").attr("value", "1");
+	$("#sldrSpr").attr("value", "2");
+	$("#sldrSum").attr("value", "3");
+});
+//variables
+
+//slider-set-with-button
+function sliderSet(){
+
+}
+
+//add-all-slider-values
+function sldrValSum(){
+}
+
+//store-sliders-as-vars
+function sliderVars(){
+
+}
+
+
+//enable-disable
+function enableSisable(){
+
+}
+
+//calculator
+function calculator(){
+
+}
+
+//cheatcodes
+function cheatcodes(){
+
+}
